92549557c17a09daa039d5c0d414c1bd5a602160
RiteshPandey2003 <riteshpandey72060@gmail.com>
2025-06-04 20:02:59 +0530

    saving data @Entity annotation use

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -14,9 +15,15 @@ public class Main {
         s1.setsName("ram");
 
         Configuration cf = new Configuration();
+        cf.addAnnotatedClass(org.example.Student.class); // for this we enable annotaion first annotaion is Entity
+        cf.configure();
         SessionFactory sf = cf.buildSessionFactory();
         Session session = sf.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
         session.save(s1);
+        transaction.commit();
 
         System.out.print(s1);
     }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Student {
 
+    @Id
     private int rollNo;
     private int sAge;
     private String sName;
